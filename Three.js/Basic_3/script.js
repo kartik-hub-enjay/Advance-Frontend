e530e2382b470339f6c66c93241a76225166e535
kartik-hub-enjay <kartikpareek6789@gmail.com>
2026-04-20 22:54:50 +0530

    done with basics of material and textures

--- Three.js/Basic_3/script.js
+++ Three.js/Basic_3/script.js
@@ -3,8 +3,9 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
 
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color :0x00ff00 , wireframe:true})
-const cube = new THREE.Mesh(geometry,material);
+const texture = new THREE.TextureLoader().load('image.jpg');
+const material = new THREE.MeshBasicMaterial({ map: texture, wireframe: true })
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 5; //This moves the camera away from objects so you can see them
 
@@ -12,22 +13,13 @@ const canvas = document.querySelector('#draw')
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(window.innerWidth,window.innerHeight);
 
-// Making our Scene resposive , whenever window size will resize this event will listen and update the matrix
-window.addEventListener('resize',()=>{
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-})
 
-//----------> orbit controls 
-// const controls = new OrbitControls(camera,renderer.domElement);
-// controls.enableDamping = true; // make s the orbit control smooth
-// controls.enableZoom = true;
-// controls.autoRotate = 0.2 ; //true 
+
+
 
 function animate(){
     window.requestAnimationFrame(animate);// a fast recursive call  (acc to fps of your system) changes the rotation very fast , that creates a visual of motion
-    // controls.update()
+  
     renderer.render(scene,camera);
     
     cube.rotation.x += 0.01;
