5415f464667525a8b886ee1795ba89032fc892b2
kartik-hub-enjay <kartikpareek6789@gmail.com>
2026-04-20 20:51:22 +0530

    Understood the basics of three.js and 3D modeling

--- Three.js/Basic_1/script.js
+++ Three.js/Basic_1/script.js
@@ -5,14 +5,14 @@ const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color :0x00ff00})
 const cube = new THREE.Mesh(geometry,material);
 scene.add(cube);
-camera.position.z = 5;
+camera.position.z = 5; //This moves the camera away from objects so you can see them
 
 const canvas = document.querySelector('#draw')
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 function animate(){
-    window.requestAnimationFrame(animate);
+    window.requestAnimationFrame(animate);// a fast recursive call  (acc to fps of your system) changes the rotation very fast , that creates a visual of motion
     renderer.render(scene,camera);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01
