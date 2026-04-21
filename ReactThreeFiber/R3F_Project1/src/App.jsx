3359db4088cd9f923120216e66962375cd015f31
kartik-hub-enjay <kartikpareek6789@gmail.com>
2026-04-21 10:08:54 +0530

    completed project1 with React3Fiber

--- ReactThreeFiber/R3F_Project1/src/App.jsx
+++ ReactThreeFiber/R3F_Project1/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import {Canvas} from '@react-three/fiber'
 import * as THREE from "three"
 import { OrbitControls , useTexture } from '@react-three/drei'
+import {Bloom,EffectComposer} from "@react-three/postprocessing"
 import Scene from './Scene'
 
 function App() {
@@ -9,10 +10,18 @@ function App() {
 
   return (
     <>
-      <Canvas camera={{fov:35}}>
+      <Canvas flat camera={{fov:35}}>
         <OrbitControls/>
         <ambientLight/>
         <Scene/>
+        <EffectComposer>
+          <Bloom
+          minmapBlur
+          intensity={5.0}
+          luminanceThreshold={0.2}
+          luminanceSmoothing={0.2}
+          />
+        </EffectComposer>
       </Canvas>
     </>
   )
--- ReactThreeFiber/R3F_Project1/src/Scene.jsx
+++ ReactThreeFiber/R3F_Project1/src/Scene.jsx
@@ -1,16 +1,22 @@
-import React from "react";
-import {Canvas} from '@react-three/fiber'
+import React, { useRef } from "react";
+import {Canvas, useFrame} from '@react-three/fiber'
 import { OrbitControls , useTexture } from '@react-three/drei'
-import nike from "../src/nike.png"
+import image from "../src/image.png"
 import * as THREE from "three"
 const Scene = () => {
-    let tex = useTexture(nike)
+    let tex = useTexture(image)
+    let cylRef = useRef(null)
+    useFrame((state,delta)=>{
+      cylRef.current.rotation.y += delta
+    })
   return (
-    
-      <mesh rotation={ro}>
-        <cylinderGeometry args={[1, 1, 1, 30, 30, true]} />
+
+      <group rotation={[0,1.5,0.5]}>
+        <mesh ref={cylRef}>
+        <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
         <meshStandardMaterial map={tex} side={THREE.DoubleSide} />
       </mesh>
+      </group>
    
   );
 };
